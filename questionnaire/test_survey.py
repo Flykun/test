e3cd52af9c8912ba1783aa37f2f1f002ffe0cebe
Flykun <1689890728@qq.com>
2018-10-24 01:00:00 +0800

    添加了setUp方法, 优化代码

--- questionnaire/test_survey.py
+++ questionnaire/test_survey.py
@@ -5,24 +5,25 @@ from questionnaire.survey import AnonymousSurvey
 class TestAnonymousSurvey(unittest.TestCase):
     '''测试AnonymousSurvey类'''
 
+    def setUp(self):
+        '''创建一个调查对象和一组答案, 供使用的测试方法使用'''
+        question = '你想首先学习哪种外语?'
+        self.my_survey = AnonymousSurvey(question)
+        self.responses = ['英语', '法语', '西语']
+
     def test_store_single_response(self):
         '''测试单个答案是否会被保存'''
-        question = '你想首先学习哪种外语?'
-        my_survey = AnonymousSurvey(question)
-        my_survey.store_response('英语')
-
-        self.assertIn('英语', my_survey.responses)
+        self.my_survey.store_response(self.responses[0])
+        self.assertIn(self.responses[0], self.my_survey.responses)
 
     def test_store_many_response(self):
         '''测试多个答案是否会被保存'''
-        question = '你想首先学习哪种外语?'
-        my_survey = AnonymousSurvey(question)
-        responses = ['英语', '法语', '西语']
-        for response in responses:
-            my_survey.store_response(response)
 
-        for response in responses:
-            self.assertIn(response, my_survey.responses)
+        for response in self.responses:
+            self.my_survey.store_response(response)
+
+        for response in self.responses:
+            self.assertIn(response, self.my_survey.responses)
 
 
 if __name__ == '__main__':
